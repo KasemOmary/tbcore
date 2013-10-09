c1eccfda57840001f10022288b9a2fc528cae7d1
Kyanwan's Code <kasemo@gmail.com>
2013-10-09 02:47:52 -0400

    Updates
    Put some meat on the bones, just a few fibers.  Modified the test
    program into a rudimentary testing frontend.

--- tbtest.pl
+++ tbtest.pl
@@ -4,11 +4,35 @@ use tbcore;
 
 my $tb = new tbcore(101);
 
-$tb->stdout_export;
-print "-----\n";
-$tb->order_set_total(100);
-$tb->stdout_export;
-print "-----\n";
-$tb->order_tax(6.35);
-$tb->stdout_export;
-print "-----\n";
+print "Local Test \n"; 
+print "a - add\n";
+print "s - subtract\n";
+print "v - view\n";
+print "x - exit\n";
+
+my $x = 1; 
+
+while ($x) {
+	print "> ";
+	my $option = <STDIN>;
+	chomp $option; 
+	if ($option =~ /x/) {
+		$x = 0;
+	} elsif($option =~ /a/) { 
+		print "% add %\n";
+	} elsif($option =~ /s/) { 
+		print "% subtract %\n";
+	} elsif($option =~ /v/) { 
+		print "% view %\n";
+	}
+}
+
+
+# $tb->stdout_export;
+# print "-----\n";
+# $tb->order_set_total(100);
+# $tb->stdout_export;
+# print "-----\n";
+# $tb->order_tax(6.35);
+# $tb->stdout_export;
+# print "-----\n";
