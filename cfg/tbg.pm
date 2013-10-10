20d5ab9456fe90c978484e288c2fa5843d8d86c4
Kyanwan's Code <kasemo@gmail.com>
2013-10-10 17:38:06 -0400

    Daily Updates.
    Updates Daily.

---
+++ cfg/tbg.pm
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# Our Glob of Globs
+
+$GV_TAX_RATE = '6.35';
+$GV_ESTABLISHMENT_NAME = 'The Test Place';
+$GV_MAX_VALUE = '100.00';
+$GV_CONTACT_MAIL = 'admin@somewhere.com';
+$GV_CONTACT_PHONE = '888-555-1234';
+
+1;
